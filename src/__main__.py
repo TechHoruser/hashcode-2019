f9c892d0808399c6c63c3abdd2cb62ee3cce818f
Unknown <joseantonio.caravacadiosdado@gmail.com>
2019-02-28 21:25:13 +0100

    cuerpo bucle

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,14 @@
 from time import sleep
 from src.ManagerFile import ManagerFile
-from src.Algorithms import BaseAlgorithms
+from src.Algorithms import *
 from src.Entities import *
 
 examples = [
-    {'fileName': 'a_example', 'algorithm': 'basic', },
+    # {'fileName': 'a_example', 'algorithm': 'basic', },
     {'fileName': 'b_lovely_landscapes', 'algorithm': 'basic', },
-    {'fileName': 'c_memorable_moments', 'algorithm': 'basic', },
-    {'fileName': 'd_pet_pictues', 'algorithm': 'basic', },
-    {'fileName': 'e_shiny_selfies', 'algorithm': 'basic', },
+    # {'fileName': 'c_memorable_moments', 'algorithm': 'basic', },
+    # {'fileName': 'd_pet_pictues', 'algorithm': 'basic', },
+    # {'fileName': 'e_shiny_selfies', 'algorithm': 'basic', },
 ]
 
 def transformInputArray(lines):
@@ -31,10 +31,33 @@ for indx, example in enumerate(examples):
 
     photoList = transformInputArray(inputArray)
 
+    #TODO creamos slides
+    slides = []
+    for photo in photoList:
+        #if photo.type == Photo.H:
+        slides.append(Slide(photo))
+        #else
+        #    buscar
+
+    #comenzamos en la primera y buscamos en bucle la proxima que daria ams puntuacion
+
+    solucion = [slides[0]]
+    slideActual = slides[0]
+
+    while len(solucion) != len(slides):
+        maxSlide = None
+        maxPuntuacion = -1
+        for slideComparando in slides:
+            if Utils.calcScoreBetweenSlides(slideActual, slideComparando) > maxPuntuacion:
+                maxSlide = slideComparando
+                maxPuntuacion = Utils.calcScoreBetweenSlides(slideActual, slideComparando)
+        solucion.append(maxSlide)
+        slideActual = maxSlide
+    slideShow = SlideShow(solucion)
     solution = eval('BaseAlgorithms.'+example['algorithm']+'(inputArray)')
 
-    outputArray = solution
-
-    mf.saveFile(outputArray)
+    # outputArray = solution
+    #
+    # mf.saveFile(outputArray)
 
     sleep(0.5)
