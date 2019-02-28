15053920a57f817e91eafb733e01f0948fae0efa
Francisco Javier Ponce Rosales <fjponce@atsistemas.com>
2019-02-28 15:58:10 +0100

    Añadidas entidades

--- src/Entities.py
+++ src/Entities.py
@@ -12,10 +12,50 @@ class Coordinate:
 
         return returnedArray
 
-class InputElementsCollection:
+class Stage:
     def __init__(self):
-        None
+        self.drones = []
+        self.warehouses = []
+        self.orders = []
 
-class OutputElementCollection:
-    def __init__(self):
-        None
+class Product:
+    def __init__(self, id, weight):
+        self.id = id
+        self.weight = weight
+
+class Drone:
+    MAX_WEIGHT = 1
+
+    def __init__(self, id, coordinate):
+        self.id = id
+        self.coordinate = coordinate
+        self.actualWeight = 0
+        self.products = []
+        self.working = 0
+
+    def addProducts(self, products):
+        weightOfProducts = sum([product.weight for product in products])
+        if self.actualWeight + weightOfProducts > Drone.MAX_WEIGHT:
+            raise Exception('Peso maximo del dron '+str(self.id)+' superado')
+
+        self.products.extend(products)
+        self.actualWeight += weightOfProducts
+
+class Warehouse:
+    def __init__(self, id, coordinate, products = []):
+        self.id = id
+        self.coordinate = coordinate
+        self.products = products
+
+    def addProducts(self, products):
+        self.products.extend(products)
+
+    def rmProducts(self, products):
+        self.products = [product for product in self.products if product.id not in products]
+
+class Order:
+    def __init__(self, id, coordinate, products = []):
+        self.id = id
+        self.coordinate = coordinate
+        self.products = products
+        self.done = False
